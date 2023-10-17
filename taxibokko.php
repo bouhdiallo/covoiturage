3465e7f367751110b4dbefb5b4765c4037839429
Bouhdiallo <bouhdiallo22@gmail.com>
2023-10-17 16:20:59 +0000

    creation formulaire avec html et css

---
+++ taxibokko.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+        <form action="">
+            <div id="general">
+            <div id="etape1">
+                <h1>Connexion</h1>
+                <p>Votre chauffeur en un clic!</p>
+
+                <button id="button">Continuer avec facebook</button><br><br>
+                
+                <div class="hr"><p>ou</p></div>
+                
+                
+
+
+                <label for="email">email</label><br>
+                <input type="text" placeholder="mara@gmail.com" id="mail"><br><br>
+                <label for="psw">Mot de pass</label><br>
+                <input type="text" placeholder="********" id="mtp"><br><br><br><br>
+
+              <a href="">j'ai deja un compte</a>
+            <button class="inscription">Se connecter--></button>
+            </div>
+
+
+            <div id="etape2">
+                <h1>Inscription</h1>
+                <p>Finaliser votre inscription en renseignants les informations manquantes</p><br><br>
+                
+                <label for="prenom">Prenom</label>
+                <input type="text" placeholder="mara">
+                <label for="nom">Nom</label>
+                <input type="text" placeholder="fall"><br><br>
+                <label for="numero">Telephone</label><br><br>
+                <span>sénégal  +221</span>
+                <input type="number" placeholder="77 000 00 00" id="phone"><br><br><br>
+                <label for="email">Email</label>
+                <input type="text" placeholder="mara10@gmail.com" class="email"><br><br><br>
+                <label for="psw">Mot de pass</label><br>
+                <input type="text" placeholder="********" id="mtp"><br><br>
+                <label for="psw">Confirmmer mot de pass</label><br>
+                <input type="text" placeholder="********" id="mtp">
+
+            <p><img src="images/delete_forever_FILL0_wght400_GRAD0_opsz24.png" alt="icons"> <a href="#">Ajouter un code promo</a></p>
+            
+            <button class="inscription">S'inscrire --></button>
+            </div>
+          </div>
+        </form>
+</body>
+</html>
