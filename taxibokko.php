6951e0be95d7176f55ba9b2e4927f3544ac9ed80
Bouhdiallo <bouhdiallo22@gmail.com>
2023-10-19 12:19:32 +0000

    creation compte client plus inscription

--- taxibokko.php
+++ taxibokko.php
@@ -7,21 +7,18 @@ $database = 'taxibokko';
 //on etablie la connexion
 $conn = new PDO ("mysql:host=$servername;dbname=$database", $username,$password);
 
-//on verifie si les chapms sont definis
+//on verifie si les champs sont definis
 if(isset($_POST['sinscrire'])){
-
     $nom = $_POST['nom'];
     $prenom =  $_POST['prenom'];
     $telephone =  $_POST['telephone'];
     $email =  $_POST['email'];
     $mot_de_pass =  $_POST['mot_de_pass'];
 
-
-//on fait une insertion 
 $sql = ("INSERT INTO `inscription`(`nom`, `prenom`, `telephone`, `email`, `mot_de_pass`) VALUES (:nom,:prenom,:telephone,:email,:mot_de_pass)");
-// on cree une requete preparée
+// on prepare la requete sql
  $a = $conn ->prepare($sql);
-//on lie les marqueurs avec des valeuurs 
+//on lie les espaces reservés a l'aide des marqeurs avec des valeuurs 
  $a-> bindParam(':nom',$nom);
  $a-> bindParam(':prenom',$prenom);
  $a-> bindParam(':telephone',$telephone);
@@ -29,26 +26,24 @@ $sql = ("INSERT INTO `inscription`(`nom`, `prenom`, `telephone`, `email`, `mot_d
  $a-> bindParam(':mot_de_pass',$mot_de_pass);
  //on execute la requete
  $a->execute();
-
 };
 
-
-
-
+//on verifie si les champs sont definis
 if(isset($_POST['connexion'])){
     $mail = $_POST['email'];
     $mot_de_pass =  $_POST['mot_de_pass'];
 
-
    $sql2 = ("SELECT  `email`, `mot_de_pass` FROM `inscription` WHERE email = :email AND mot_de_pass = :mot_de_pass");
-
+// on prepare la requete sql
    $b = $conn->prepare($sql2);
-
+//on lie les espaces reservés a l'aide des marqeurs avec des valeuurs 
    $b->bindParam(':email',$mail);
    $b->bindParam(':mot_de_pass',$mot_de_pass);
+    //on execute la requete
    $b->execute();
 
-   $resultat=$b->fetch(PDO::FETCH_ASSOC);
+   //on recupere les resultats 
+   $resultat=$b->fetch(PDO::FETCH_ASSOC); 
   
    if($resultat) {
     echo "ce compte existe";
@@ -56,8 +51,46 @@ if(isset($_POST['connexion'])){
    } else {
     echo "ce compte n'existe pas";
    }
-
 }
+?>
+
+<?php
+
+/*
+// Vérifier que tous les champs sont remplis
+if (empty($nom) || empty($prenom) || empty($telephone) || empty($email) || empty($mot_de_pass)) {
+    echo "Tous les champs doivent être remplis.";
+} else {
+    // Vérifier que le nom et le prénom contiennent que des lettres et ne dépassent pas 10 caractères
+    if (ctype_alpha($nom) && ctype_alpha($prenom) && strlen($nom) <= 10 && strlen($prenom) <= 10) {
+        // Vérifier que le numéro de téléphone ne dépasse pas 9 chiffres et commence par "7"
+        if (is_numeric($telephone) && strlen($telephone) <= 9 && substr($telephone, 0, 1) == "7"){ 
+
+        } 
+
+// Vérifiez que les noms et prénoms ne contiennent que du texte
+if (ctype_alpha($nom) && ctype_alpha($prenom)) {
+    echo "<br>" ;
+}else{ 
+    echo "Les noms et prénoms ne doivent contenir que des lettres.";
+}
+    // Vérifiez que les noms et prénoms ne dépassent pas 10 caractères
+    if (strlen($nom) > 10 && strlen($prenom) > 10) {
+        echo "Les noms et prénoms ne doivent pas dépasser 10 caractères chacun.";
+    }
+        //verifer que le numero est numerique
+ if(is_numeric($telephone) && substr($telephone, 0, 1) == "7"){ 
+    echo "le numero de telephone est valide";
+ } else {
+    echo "le numero de telephone doit etre numerique et doit imperativement commencer par un '7'";
+ }
+
+} 
+if(empty($nom)|| empty($prenom)|| empty($telephone)|| empty($email)|| empty($mot_de_pass)){
+    echo "tous les champs sont obliagtoires";
+} else{ 
+
+}*/
 
 ?>
 
@@ -109,7 +142,7 @@ if(isset($_POST['connexion'])){
                 <label for="psw">Mot de pass</label><br>
                 <input type="text" placeholder="********" id="mtp" name = "mot_de_pass"><br><br>
                 <label for="psw">Confirmmer mot de pass</label><br>
-                <input type="text" placeholder="********" id="mtp" name = "mot_de_pass">
+                <input type="text" placeholder="********" id="mtp" name = "retaper">
 
             <p><img src="images/delete_forever_FILL0_wght400_GRAD0_opsz24.png" alt="icons"> <a href="#">Ajouter un code promo</a></p>
             
